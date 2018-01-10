fc364347dd78695b1e0d97f5ffe76b46aedc9f00
Tin Chu <mchu.tinho@gmail.com>
2018-01-10 11:59:48 -0600

    easy queries

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const {app, BrowserWindow} = require('electron');
-let win
+const { app, BrowserWindow } = require('electron');
 
-app.on('ready', function() {
-  win = new BrowserWindow({width: 800, height: 600});
+let win;
+
+app.on('ready', () => {
+  win = new BrowserWindow({ width: 800, height: 600 });
 
   win.loadURL(`file://${__dirname}/index.html`);
 });
